23ba1d63f5973ecf7b4db0951ef8f7bed1228751
douglas.oliveira <douglasoliveira320@gmail.com>
2025-07-26 09:39:41 -0300

    deploy 6

--- src/infra/typeORM/config.ts
+++ src/infra/typeORM/config.ts
@@ -5,22 +5,13 @@ import { CategoryEntity } from "./entities/category";
 
 import "dotenv/config";
 
-// Log das configurações de conexão (removendo a senha por segurança)
-const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  port: 3306,
-  username: process.env.DB_USER,
-  database: process.env.DB_NAME || "produtos_service",
-};
-console.log("Database configuration:", dbConfig);
-
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST || "localhost",
   port: 3306,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "produtos_service",
+  database: "produtos_service",
   logging: true,
   synchronize: false,
   entities: [CategoryEntity, ProductEntity],
